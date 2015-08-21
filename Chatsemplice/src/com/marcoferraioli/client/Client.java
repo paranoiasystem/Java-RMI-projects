737432fa0a4895f32b59750455401a08273ad6a3
ParanoiaSystem <m.ferraioli25@studenti.unisa.it>
2015-08-21 12:57:37 +0200

    init commit

---
+++ Chatsemplice/src/com/marcoferraioli/client/Client.java
@@ -0,0 +1,10 @@
+package com.marcoferraioli.client;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Client extends Remote {
+	public void detto (String username, String message) throws RemoteException;
+	public void iscritto (String username) throws RemoteException;
+	public void abbandonato (String username) throws RemoteException;
+}
